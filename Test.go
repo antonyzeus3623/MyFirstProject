218887fb366e8d1da380635858f9438575e9a888
antonyzeus3623 <wangjie3623.boe.com.cn>
2021-12-17 16:24:19 +0800

    my first commit

---
+++ Test.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func testSlice() {
+	var slice []int
+	var arr [5]int = [...]int{1, 2, 3, 4, 5}
+
+	slice = arr[:4]
+	fmt.Println(slice)
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
+
+}
+
+func main() {
+	testSlice()
+}
