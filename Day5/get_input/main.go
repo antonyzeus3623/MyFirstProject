758437370cf0edbc426cc8a40ba755f46cb448bd
antonyzeus3623 <antonyzeus@163.com>
2022-05-07 16:54:58 +0800

    获取用户输入，带空格

--- Day5/get_input/main.go
+++ Day5/get_input/main.go
@@ -10,9 +10,9 @@ import (
 func userBufio() {
 	fmt.Println("请输入：")
 	var str string
-	reader := bufio.NewReader(os.Stdin) // 获取标准输入
+	reader := bufio.NewReader(os.Stdin) // 获取标准输入  NewReader的参数就是接口类型
 	str, _ = reader.ReadString('\n')    // ReadString('\n')：读到回车表示输入结束，会返回内容str和io.EOF。
-	fmt.Println(str)
+	fmt.Printf("你输入的内容是：%s\n", str)
 }
 func main() {
 	userBufio()
